9c11163ad1d5370609db69c38848f4220ec76054
Tono <antonio.nogueras@yahoo.com>
2017-10-25 19:20:41 -0500

    Update add_column_data_to_new_csv.py

--- add_column_data_to_new_csv.py
+++ add_column_data_to_new_csv.py
@@ -6,18 +6,20 @@ with open('OLD_CSV_FILE_NAME_GOES_HERE.csv', 'r') as csv_file:
 	with open('NEW_CSV_FILE_NAME_GOES_HERE.csv', 'w') as target_csv:
 		theWriter = csv.writer(target_csv)
     
-    #Skip the column name
-		next(csv_reader)
+   
+		next(csv_reader)  #Skip the column name
+		
 		for line in csv_reader:
-
-			#Index of the column
-			columnIndex = line[54]
+	
+			columnIndex = line[54] #Index of the column
 
 			if bool(bulletOne) == True:
         
-        # Wrapping the column's row data with <li></li> tags that contains data, 
-        
-        # but skips a cell with no data
+        """ 
+	Wrapping the column's row data with <li></li> tags that contains data, 
+     	    but skips a cell with no data. The else can be replaced with 'pass'
+	    if you don't mind putting something in that cell.
+	 """
 				
         theWriter.writerow(
 				[
