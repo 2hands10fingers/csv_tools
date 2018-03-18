12bc3501b988cef35505f32b9096ec9cd88adf4a
Tono <antonio.nogueras@yahoo.com>
2018-03-18 14:14:39 -0500

    Update picsorter.py

--- picsorter.py
+++ picsorter.py
@@ -17,43 +17,56 @@ def nameshorten(image):
     name_length = len(image)
     namediv_even = name_length / 2
     namediv_odd = int(ceil(name_length / 2))
+    even_name_first_half = image[:namediv_even - namediv_even / 2 ]
+    even_name_second_half = image[- int(ceil(namediv_even / 2)):]
+    odd_name_first_half = image[:namediv_odd - namediv_odd / 2 ]
+    odd_name_second_half = image[- int(ceil(namediv_odd / 2)):]
 
     if name_length % 2 == 0 and name_length > 50: #even
-        return "{} ... {} ".format(image[:namediv_even - namediv_even / 2 ], image[- int(ceil(namediv_even / 2)):])
+        return f"{even_name_first_half} ... {even_name_second_half}"
     elif name_length % 2 != 0 and name_length > 50: #odd
-        return "{} ... {} ".format(image[:namediv_odd - namediv_odd / 2 ], image[- int(ceil(namediv_odd / 2)):])
+        return f"{odd_name_first_half} ... {odd_name_second_half}"
     else:
         return image
 
 def imagesize(x):
     width = str(image.open(x).width)
     height = str(image.open(x).height)
-    return "{}x{}".format(width, height)
+    
+    return f"{width}x{height}"
 
 for i in current_directory:
+    
     try:
         fileformat = image.open(i).format
         imgsize = imagesize(i)
+        
         if not path.exists(imgsize):
             mkdir(imgsize)
             folders.append('.')
+        
         else:
-            print "Image: {} -> Folder: '{}'".format(nameshorten(i), imgsize)
+            
+            print(f"Image: {nameshorten(i)} -> Folder: '{imgsize}'")
+    
     except IOError:
         continue
 
 for i in current_directory:
+    
     try:
         imgsize = imagesize(i)
         fileformat = image.open(i).format
+        
         if fileformat in ('JPEG', 'BMP', 'GIF', 'PNG'):
             mover(i, imgsize)
             files.append('*')
+    
     except IOError:
         continue
 
-print "------------------"
-print "  \n Sorting complete!\n"
-print "- Files sorted: {}".format(len(current_directory))
-print "- Images moved: {}".format(len(files))
-print "- Folders created: {}".format(len(folders))
+print("-"*60)
+print("  \n Sorting complete!\n")
+print(f"- Files sorted: {len(current_directory)}")
+print(f"- Images moved: {len(files)}")
+print(f"- Folders created: {len(folders)}")
