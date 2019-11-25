cf8a76aef5cc2ab479ada7ed5ed9437bf57800d6
Tono <antonio.nogueras@yahoo.com>
2019-11-24 21:19:26 -0600

    Update web_scraper.py

--- web_scraper.py
+++ web_scraper.py
@@ -1,50 +1,109 @@
 from bs4 import BeautifulSoup as bs
-from requests import get
-import re
-
+from requests import get, post, Session
+import os
+from datetime import datetime
 
 class Scraper:
-	def __init__(self, url, parser=None, headers=None):
-		self.url = url
-		self.parser = parser if parser else 'lxml'
-		self.headers = headers if headers else self.default_headers()
-		self.status = get(self.url).status_code
-		self.result = None 
+  def __init__(self, url=None, parser=None, headers=None, www=False, ssl=False, domain=''):  #nopep8
+    self.domain = "" if not domain else domain.replace(".", "")
+    self.www = '' if not www else 'www.'
+    self.ssl = "http://" if not ssl else "https://"
+    self.url = None if not url else f'{self.ssl}{self.www}{url}{self.domain}' #nopep8
+    self.parser = parser if parser else 'lxml'
+    self.headers = headers if headers else self.default_headers()
+    self.status = get(self.url, self.headers).status_code
+    self.results = None
+
+  
+  def lprint(self, prop=None):
+
+    if prop and hasattr(self, f"get_{prop}"):
+      for i in getattr(self, f"get_{prop}")():
+        try:
+          print(i)
+        except TypeError:
+          continue
 
 
-	def default_headers(self):
-	    ua_one = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) '
-	    ua_two = 'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'
-	    headers = {'User-Agent': ua_one + ua_two}
+    else:
+      if self.results:
+        for i in self.results:  # nopep8
+          print(i)
 
-	    return headers
+    return self
 
-	def get_slug(self):
-		return self.split('/')[-1]
+  def default_headers(self):
+      ua_one = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) '
+      ua_two = 'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'
+      headers = {'User-Agent': ua_one + ua_two}
 
-	def download_all(self, write_bytes=True, parser=None):
+      return headers
 
-		def download(file_name, file_url):
-			with open(file_name, 'wb' if write_bytes else 'w') as file:
-				print(f'Saving file name: {file_name}')
-				file.write(get(file_url).content)
+  def scrape(self, element=None, attr=None, attr_name=None, style="", msg=True):
+    
+    if msg:
+      print(f'SCRAPING {self.url}\n')
+    
+    if isinstance(attr, str) and attr_name == None or isinstance(attr_name, str) and attr == None:
+       raise SystemExit('\nMake sure attr and attr_name are set.')
 
+    def styled_request(style):
+      request = get(self.url, headers=self.headers)
+      if style == "json":
+        return request.json()
+      elif style == "content":
+       return request.content
+      
+      return request.text
 
-		if isinstance(self, (list, tuple, set)):
-			for i in self:
-				download(i.split('/')[-1] if not parser else parser(i), i)
+    soup = bs(styled_request(style), self.parser)
+    self.results = soup.find_all(element, { attr : attr_name })
+    return self
 
-	def get_piece(self, element=None, attribute=None, attribute_name=None, 
-				  operation=None, download_all=False, parser=None, write_bytes=True):
+  def download_images(self, file_path, sub_folder=None):
+    
+    if self.images:
+      folder = os.path.join(file_path, sub_folder) if sub_folder != None else file_path
+      
+      if not os.path.exists(folder):
+          os.makedirs(folder)
+      
+      for img in self.images:
+        img_name = img.split("/")[-1]
+        print("DOWNLOADING: ", img_name)
+        
+        with open(f'{os.getcwd()}/{folder}/{img_name}', 'wb') as f:
+          response = get(img, headers=self.headers)
 
-		soup = bs(get(self.url, headers=self.headers).text, self.parser)
-		result = soup.find_all(element, { attribute : attribute_name })
-		self.result = operation(result) if operation != None else result
+          if not response.status_code == 200: #OK
+            continue
 
-		return Scraper.download_all(self.result, parser=parser, write_bytes=write_bytes) if download_all else self.result
+          f.write(response.content) 
 
-def parsed(soup_result):
-	return [ Scraper.get_slug(i.img["src"]) for i in soup_result]
-		
+  def get(self, prop=None):
+    if prop and hasattr(self, prop):
+      print(getattr(self, prop))
+      
+  def get_links(self):
+    if self.links:
+      return list(self.links)
 
-print(Scraper('https://website.com').get_piece(element='div', attribute="class", attribute_name="class_name", operation=parsed))
+  def get_results(self):
+    if self.results:
+      return self.results
+  
+  def get_images(self):
+    if not self.images:
+      return self.images
+       
+  def set_links(self):
+     if self.results != None:
+      self.links = map(lambda x: x.a["href"], self.results)
+      
+      return self
+
+  def set_images(self):
+    if self.results != None:
+      self.images = map(lambda x: x.img["src"], self.results)
+      
+      return self
